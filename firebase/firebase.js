913c12b1cefda562335c99c5f7b705c57709a85c
Ayesha <87906936+ayeshaa7@users.noreply.github.com>
2023-02-15 12:43:58 +0530

    Update firebase.js

--- firebase/firebase.js
+++ firebase/firebase.js
@@ -1,14 +1,7 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth"; //get current user info who's logged in for databasr also, protect route as only when user is logged in they can certain things 
+import { getAuth } from "firebase/auth"; //get current user info who's logged in for database
 import { getFirestore } from "firebase/firestore"; //database, to retrieve, write, update, use 
 
-
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_API_KEY,
   authDomain: process.env.NEXT_PUBLIC_AUTH_DOMAIN,
